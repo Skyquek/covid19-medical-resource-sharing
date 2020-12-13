cd74843784b23affe85ea41b02fb421a4541a2e6
henryyap98 <zhongheng98@hotmail.com>
2020-12-13 21:01:41 +0800

    Optimize the donation interface

--- src/View/donation.php
+++ src/View/donation.php
@@ -10,15 +10,14 @@
 	<link rel="stylesheet" href="../../assets/css/include.css">
 </head>
 
-<body>
-
+<body style="background-color:white">
 <?php include("includes/navigation.php");?>
-
 <?php include("includes/header.php");?>
-<div class="text-center"> 
-	<h1 style="color:white">Donate</h1>
-</div>
-
+<h1 style="font-weight:bold" class="text-center">Donate to help each other</h1><br>
+<p class="text-center">By making a much needed donation you will join us to comfirm  and find the solutions we need to our most important environmental issues.</p>
+<p></p>
+<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;margin-left:900px">Donate</button><br>
+<p></p>
 
 
 <?php include("includes/footer.php");?>
