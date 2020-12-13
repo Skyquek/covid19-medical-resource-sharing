c995fc09049749116878996bbf6756df63c4e90f
Yao Jing Quek <yaojingquek@gmail.com>
2020-12-13 14:32:13 +0800

    Update the directory structure of the project space

---
+++ src/View/about.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="../../assets/css/include.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="alert alert-warning alert-dismissible fade show text-center"style="color:black;margin-bottom:0px">
+	<button type="button" class="close" data-dismiss="alert">&times;</button>
+	<strong>Warning!</strong> visit <a href="http://covid-19.moh.gov.my/" class="alert-link">Kementerian Kesihatan Malaysia</a> to get latetest information
+</div>
+<nav class="navbar navbar-expand-sm navbar-dark justify-content-center sticky-top" style="margin-top:0px" data-spy="affix" >
+	<ul class="navbar-nav">
+		<li class="nav-item " style="color:white">
+		<a class="nav-link" href="home.html" >Home</a>
+		</li>
+	<li class="nav-item" style="color:white">
+		<a class="nav-link active" href="about us.html" >About Us</a>
+	</li>
+	<li class="nav-item" style="color:white">
+		<a class="nav-link" href="donation.html">Donation</a>
+	</li>
+	<li class="nav-item dropdown">
+			<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Account</a>
+				<div class="dropdown-menu dropdown-menu-right">
+					<a class="dropdown-item" href="signin.html">Sign In</a>
+					<a class="dropdown-item" href="register.html">Sign Up</a>
+					<a class="dropdown-item" href="forget.html">Forget Password</a>
+				</div>
+		</li>  
+	</ul>
+</nav>
+<div class="jumbotron text-center" style="margin-bottom:0px;margin-top:0px">
+<h1 style="font-size:50px;color:white">Contribute to help other</h1>
+<p style="font-size:15px;color:white">Help us provide resources for the fight against Covid-19</p> 
+</div>
+<nav class="navbar justify-content-end" style="background-color:gray;height:20px"></nav><br>
+<div class="text-center"> <h1 style="color:white">About Us</h1><p style="color:white"> Our Team is formed by 4 student who <p>
+<img src="../../assets/img/logo.png" class="rounded" alt="Cinque Terre">
+<div class="container">
+	<div class="row">
+		<div class="col-lg-6" style="margin-left:140px">
+			<div class="card" style="width:400px">
+			<div class="card-body">
+				<h4 class="card-title">Quak Yao Jing</h4>
+				<p class="card-text">Our Team Leader,  who develop backend of this website</p>
+				<a href="#" class="btn btn-primary">See Profile</a>
+			</div>
+			</div>
+		<br>
+			<div class="card" style="width:400px">
+			<div class="card-body">
+				<h4 class="card-title">John Doe</h4>
+				<p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+				<a href="#" class="btn btn-primary">See Profile</a>
+			</div>
+			</div>
+		</div>
+	<br>
+		<div class="col-lg-6" style="margin-left:-150px">
+			<div class="card" style="width:400px">
+			<div class="card-body">
+	<h4 class="card-title">John Doe</h4>
+	<p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+	<a href="#" class="btn btn-primary">See Profile</a>
+	</div>
+</div>
+<br>
+<div class="card" style="width:400px">
+	<div class="card-body">
+	<h4 class="card-title">John Doe</h4>
+	<p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+	<a href="#" class="btn btn-primary">See Profile</a>
+	</div>
+</div>
+	</div>
+	</div>
+</div>
+</div><br>
+<div class="jumbotron3" style="margin-bottom:-10px;height:280px text-align: right">
+<h1 style="font-size:130px;color:white;font-weight:bold;margin-left:60px">Please Wash</h1>
+<p style="font-size:50px;color:white;font-weight:bold;margin-left:60px">Your Hand</p>
+</div>
+<div class="container-fluid p-2 my-1 text-center margin-bottom:0px" style="background-color:white">
+<h2 style="font-size:50px;margin-left:10px">Any Question?</h2>
+<button type="button" class="btn btn-outline-danger">Send Us Feedback</button></h2>
+</div>
+<div class="jumbotron1" style="margin-bottom:0px">
+<div class="container container-fluid text-left" >         
+<table class="table table-borderless ">
+	<tr>
+		<th style="color:white">Contact Us</th>
+	</tr>
+	<tbody>
+		<td style="color:white">Phone: xxx-xxxxxxx<br>Mobile: xxx-xxxxxxx<br>Email: zhongheng98@hotmail.com<br>Web: </td>
+		<td>
+	</tbody>
+</table>
+</div>
+</div>
+<div class="jumbotron2 text-center" style="margin-bottom:0px;height:40px">
+<p style="font-size:10px;color:white">Copyright 2018 ASHIC | All Rights Reserved | xxxxxx</p> 
+</div>
+</body>
+</html>
