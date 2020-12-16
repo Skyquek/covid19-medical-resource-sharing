267b8411979616ce045c54151b0288aba3daee5a
henryyap98 <zhongheng98@hotmail.com>
2020-12-16 16:49:45 +0800

    admin-update again

---
+++ src/View/includes/admin/adminhome.php
@@ -0,0 +1,64 @@
+<div class="main">
+	<div class="container p-3 my-4 border">
+	<h2>Overview</h2>
+	<div class="row">
+	<div class="col-sm-6">
+	<div class="container1 p-3 my-4 border"><h6>hen</h6></div>
+	</div>
+	<div class="col-sm-6">
+	<div class="container1 p-3 my-4 border"><h6>hen</h6></div>
+	</div>
+	</div>
+	<div class="row">
+	<div class="col-sm-6">
+	<div class="container1 p-3 my-4 border"><h6>hen</h6></div>
+	</div>
+	<div class="col-sm-6">
+	<div class="container1 p-3 my-4 border"><h6>hen</h6></div>
+	</div>
+	</div>
+	</div>
+	<div class="container p-3 my-4 border">
+	<h2>Request</h2>
+	<div class="row">
+	<div class="col-sm-6">
+	<div class="container1 p-3 my-4 border"><h6>hen</h6></div>
+	</div>
+	<div class="col-sm-6">
+	<div class="container1 p-3 my-4 border"><h6>hen</h6></div>
+	</div>
+	</div>
+	<div class="row">
+	<div class="col-sm-6">
+	<div class="container1 p-3 my-4 border"><h6>hen</h6></div>
+	</div>
+	<div class="col-sm-6">
+	<div class="container1 p-3 my-4 border"><h6>hen</h6></div>
+	</div>
+	</div>
+	<a href="requestment.php">see more</a>
+	</div>
+	<div class="container p-3 my-4 border">
+	<h2>Completed Task</h2>
+	<div class="row">
+	<div class="col-sm-6">
+	<div class="container1 p-3 my-4 border"><h6>hen</h6></div>
+	</div>
+	<div class="col-sm-6">
+	<div class="container1 p-3 my-4 border"><h6>hen</h6></div>
+	</div>
+	</div>
+	<div class="row">
+	<div class="col-sm-6">
+	<div class="container1 p-3 my-4 border"><h6>hen</h6></div>
+	</div>
+	<div class="col-sm-6">
+	<div class="container1 p-3 my-4 border"><h6>hen</h6></div>
+	</div>
+	</div>
+	<a href="completed.php">see more</a>
+	</div>
+	<div class="container p-3 my-4 border">
+	<h2>Data</h2>
+	</div>
+</div>
