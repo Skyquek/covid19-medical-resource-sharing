5feebac3bd1b63a4824f536effe90b327f50f1c9
Yao Jing Quek <yaojingquek@gmail.com>
2020-12-19 18:16:16 +0800

    Update the about.php

--- src/View/Main/about.php
+++ src/View/Main/about.php
@@ -14,46 +14,50 @@
 
 <?php include("../includes/main/navigation.php");?>
 <?php include("../includes/main/header.php");?><br>
-<div class="text-center"> <h1 style="color:white">About Us</h1><p style="color:white"> Our Team is formed by 4 student who <p>
+<div class="text-center"> <h1 style="color:white">About Us</h1>
+<p style="color:white"> This project is initiated by four students in University Teknikal Malaysia Melaka (UTeM). <br> They have witness how Covid-19 impact our society life and hope to contribute to the society by using this platform to connect everyone together. <p>
 
 <img src="../../../assets/img/logo.png" class="rounded" alt="Cinque Terre">
 <div class="container">
 	<div class="row">
-		<div class="col-lg-6" style="margin-left:140px">
-			<div class="card" style="width:400px">
-			<div class="card-body">
-				<h4 class="card-title">Quak Yao Jing</h4>
-				<p class="card-text">Our Team Leader,  who develop backend of this website</p>
-				<a href="#" class="btn btn-primary">See Profile</a>
+			<div class="col-lg-6" style="margin-left:140px">
+				<div class="card" style="width:400px">
+					<div class="card-body">
+						<h4 class="card-title">Quek Yao Jing</h4>
+						<p class="card-text">Yao Jing is the leader of the project and the team. He is man who spark out the idea of connecting everyone to fight covid-19 pandemic. </p>
+						<a href="#" class="btn btn-primary">See Profile</a>
+					</div>
+				</div>
+				<br>
+				<div class="card" style="width:400px">
+					<div class="card-body">
+						<h4 class="card-title">Khalid Taufik</h4>
+						<p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+						<a href="#" class="btn btn-primary">See Profile</a>
+					</div>
 			</div>
-			</div>
-		<br>
-			<div class="card" style="width:400px">
-			<div class="card-body">
-				<h4 class="card-title">John Doe</h4>
-				<p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-				<a href="#" class="btn btn-primary">See Profile</a>
-			</div>
-			</div>
-		</div>
+	</div>
+
 	<br>
 		<div class="col-lg-6" style="margin-left:-150px">
 			<div class="card" style="width:400px">
-			<div class="card-body">
-	<h4 class="card-title">John Doe</h4>
-	<p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-	<a href="#" class="btn btn-primary">See Profile</a>
-	</div>
-</div>
-<br>
-<div class="card" style="width:400px">
-	<div class="card-body">
-	<h4 class="card-title">John Doe</h4>
-	<p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-	<a href="#" class="btn btn-primary">See Profile</a>
-	</div>
-</div>
-	</div>
+				<div class="card-body">
+					<h4 class="card-title">Yap Zhong Heng</h4>
+					<p class="card-text">Zhong Heng is the core of the UI/UX design of the site and platform. He aims to develop a site that are user friendly and helpful to everyone. </p>
+					<a href="#" class="btn btn-primary">See Profile</a>
+				</div>
+			</div>
+
+			<br>
+
+			<div class="card" style="width:400px">
+				<div class="card-body">
+					<h4 class="card-title">Ashykin</h4>
+					<p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+					<a href="#" class="btn btn-primary">See Profile</a>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 
