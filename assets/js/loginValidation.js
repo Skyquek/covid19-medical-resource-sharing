f98b788d617b1535421e5ea81a0dc7a61eac7a16
Yao Jing Quek <yaojingquek@gmail.com>
2020-12-19 16:56:38 +0800

    Javascript 2nd email verification

--- assets/js/loginValidation.js
+++ assets/js/loginValidation.js
@@ -1,33 +1,48 @@
 var errorBox = document.getElementById("error-box");
-var loginID = document.getElementById("loginID");
+var email = document.getElementById("loginID");
 var loginPass = document.getElementById("loginPass");
 errorBox.style.display = "none";
 
 $(function() {
+    
     $("form#loginForm").submit(function() {
-        $.ajax({
-            url: "../../Controller/Auth/LoginController.php",
-            type: "POST",
-            data: {
-                loginid: loginID.value,
-                pass: loginPass.value
-            },
-            dataType: "json",
-            success: function(data) {
-                if(data.status == 'success')
-                {
-                    alert(data.message);
+
+        // Validate Email
+        if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email.value))
+        {
+            $.ajax({
+                url: "../../Controller/Auth/LoginController.php",
+                type: "POST",
+                data: {
+                    loginid: email.value,
+                    pass: loginPass.value
+                },
+                dataType: "json",
+                success: function(data) {
+                    if(data.status == 'success')
+                    {
+                        alert(data.message);
+                    }
+                    else
+                    {
+                        errorBox.innerHTML = "<i class='fa fa-times'></i>" + " Please Check your login ID and Password!";
+                        errorBox.style.display = "block";
+                    }
+                },
+                error: function(error) {
+                    alert(error);
                 }
-                else
-                {
-                    errorBox.innerHTML = "<i class='fa fa-times'></i>" + " Please Check your login ID and Password!";
-                    errorBox.style.display = "block";
-                }
-            },
-            error: function(error) {
-                alert(error);
-            }
-        });
-        return false;
+            });
+            return false;
+        }
+        else 
+        {
+            errorBox.innerHTML = "<i class='fa fa-times'></i>" + " Please Check your email format!";
+            errorBox.style.display = "block";
+        }
+
+        
     });
   });
+
+  
