41dbdf67f179dfdd51a2129122733c63fab36a19
Yao Jing Quek <yaojingquek@gmail.com>
2020-12-27 11:37:13 +0800

    Edit aboutus css interface

--- src/View/includes/main/aboutus.php
+++ src/View/includes/main/aboutus.php
@@ -14,6 +14,11 @@ body {
 .card {
   margin: 8px;
   width:300px;
+  height: 700px;
+}
+
+.card-content {
+  height: 180px;
 }
 
 
@@ -28,7 +33,9 @@ body {
 }
 
 .title {
-  color: white;
+  color: Black;
+  font-size: 25px;
+  font-weight: bold;
 }
 
 .button {
@@ -45,6 +52,8 @@ body {
 
 .button:hover {
   background-color: #555;
+  text-decoration: none;
+  color: white;
 }
 
 @media screen and (max-width: 650px) {
@@ -60,52 +69,90 @@ body {
 <div class="col-sm-1"></div>
   <div class="column">
     <div class="card">
-      <img src="/w3images/team1.jpg" alt="Jane" style="width:100%">
+      <img src="../../../assets/img/quek.png" alt="Quek" style="width: 300px; height: 300px;">
       <div class="container">
-        <h2>Quek Yao Jing</h2>
-        <p class="title">Leader</p>
-        <p>Who incharge this project</p>
-        <p>jane@example.com</p>
-        <p><button class="button">Profile</button></p>
+        <br>
+        <center>
+          <h2>Quek Yao Jing</h2>
+          <p class="title">Lead Programmer</p>
+          <p>B031810136@student.utem.edu.my</p>
+        </center>
+
+        <div class="card-content">
+          <p>Quek is in charge of managing the project and its direction. Timeframe, resources, coordination and other important aspects is within his responsibilities. In a technical side, he also in charge of programming. </p>
+        </div>
+
+        <center>
+          <p><a href="biography/quek/index.html" class="button">Contact Profile</a></p>
+        </center>
       </div>
     </div>
   </div>
 
   <div class="column">
     <div class="card">
-      <img src="/w3images/team2.jpg" alt="Mike" style="width:100%">
+      <img src="../../../assets/img/henry.jpg" alt="Henry" style="width: 300px; height: 300px;">
       <div class="container">
-        <h2>Mike Ross</h2>
-        <p class="title">Art Director</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>mike@example.com</p>
-        <p><button class="button">Contact</button></p>
+        <br>
+        <center>
+          <h2>Yap Zhong Heng</h2>
+          <p class="title">Front-End Designer</p>
+          <p>mike@example.com</p>
+        </center>
+        
+        <div class="card-content">
+          <p>Yap is the main front-end designer of this project. He is responsible for design innovative UI and making sure the user-friendly experience is in top condition.  </p>
+        </div>
+
+        <center>
+          <p><a class="button">Contact Profile</a></p>
+        </center>
+
       </div>
     </div>
   </div>
   
   <div class="column">
     <div class="card">
-      <img src="/w3images/team2.jpg" alt="Mike" style="width:100%">
+      <img src="/w3images/team2.jpg" alt="Mike" style="width: 300px; height: 300px;">
       <div class="container">
-        <h2>Mike Ross</h2>
-        <p class="title">Art Director</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>mike@example.com</p>
-        <p><button class="button">Contact</button></p>
+        <br>
+        <center>
+          <h2>Ashiqin</h2>
+          <p class="title">Data Analyst</p>
+          <p>mike@example.com</p>
+        </center>
+
+        <div class="card-content">
+          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+          
+        </div>
+
+        <center>
+          <p><a class="button">Contact Profile</a></p>
+        </center>
       </div>
     </div>
   </div>
   
   <div class="column">
     <div class="card">
-      <img src="/w3images/team3.jpg" alt="John" style="width:100%">
+      <img src="/w3images/team3.jpg" alt="John" style="width: 300px; height: 300px;">
       <div class="container">
-        <h2>John Doe</h2>
-        <p class="title">Designer</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>john@example.com</p>
-        <p><button class="button">Contact</button></p>
+        <br>
+        <center>
+          <h2>Taufik</h2>
+          <p class="title">QA Engineer</p>
+          <p>john@example.com</p>
+        </center>
+
+        <div class="card-content">
+          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        </div>
+        
+        <center>
+          <p><a class="button">Contact Profile</a></p>
+        </center>
       </div>
     </div>
   </div>
