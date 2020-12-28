4bb669bb8fb01ff165a1a66b2fc3b301685cca23
Yao Jing Quek <yaojingquek@gmail.com>
2020-12-28 21:51:57 +0800

    Add Category Model

---
+++ src/Model/Category.php
@@ -0,0 +1,32 @@
+<?php
+
+class Category {
+
+    public function getCategoryName($category_id="") {
+        include("connection.php");
+
+        if($category_id == ""){
+             $sql = "";
+        }
+        else{          
+            $sql = "SELECT category_name FROM category 
+                    WHERE category_id=$category_id";
+        }
+        
+        $result = $connection->query($sql);
+        if($result === TRUE) {
+            echo "";
+        }
+        else {
+            // echo "Error: " . $sql . "<br>" . $connection->error;
+        }
+        $connection->close();
+        
+        return $result;
+
+    }
+
+    
+}
+
+?>
